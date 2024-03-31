1b970562450aaeaa944014cde1553025cceb98bd
Roberto Pickart Violato <31355392+robertoviolato@users.noreply.github.com>
2024-03-31 01:03:38 -0300

    Add files via upload

---
+++ imc/script.js
@@ -0,0 +1,25 @@
+const button = document.getElementById('button');
+const nome = document.getElementById('nome');
+const peso = document.getElementById('peso');
+const altura = document.getElementById('altura');
+const resultado = document.getElementById('resultado');
+
+const getText = (imc) => {
+    if(imc > 40) return 'Obesidade grau III';
+    if(imc > 35) return 'Obesidade grau II';
+    if(imc > 30) return 'Obesidade grau I';
+    if(imc > 25) return 'Levemente acima do peso';
+    if(imc > 18.5) return 'Peso ideal';
+    if(imc > 18.5) return 'Peso ideal';
+    return 'Abaixo do peso';
+}
+
+const imcCalc = () => {
+if(!peso.value || !altura.value || !nome.value) return resultado.textContent = 'Preencha todos os campos';
+
+    const valorImc = (peso.value / (altura.value * altura.value)).toFixed(1)
+    // console.log(valorImc);
+    resultado.textContent = getText(valorImc);
+}
+
+button.addEventListener('click', imcCalc);
